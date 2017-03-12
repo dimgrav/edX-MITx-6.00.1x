2164dd7789d5f00329c71ebfd25216f3470c698b
dimgrav <dim_grav@yahoo.gr>
2017-03-13 01:15:12 +0200

    Update problem 2.py

--- problem set 1/problem 2.py
+++ problem set 1/problem 2.py
@@ -6,7 +6,7 @@ Created on Sun Mar 12 12:58:18 2017
 """
 
 counter = 0
-s = 'azcbobobegghakl
+s = 'azcbobobegghakl'
 
 for i in range(len(s)-2):
     if s[i] == "b" and s[i+1] == "o" and s[i+2] == "b":
