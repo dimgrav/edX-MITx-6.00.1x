c17734099d9fa9439bf23234c67921706bfa321b
Dimitris Gravanis <dim_grav@yahoo.gr>
2017-04-02 20:02:29 +0300

    Update ps3_hangman.py

--- problem set 3/ps3_hangman.py
+++ problem set 3/ps3_hangman.py
@@ -1,12 +1,4 @@
 # Hangman game
-#
-
-# -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
-
 import random
 
 WORDLIST_FILENAME = "words.txt"
@@ -36,11 +28,8 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
-# end of helper code
-# -----------------------------------
 
-# Load the list of words into the variable wordlist
-# so that it can be accessed from anywhere in the program
+# -----------------------------------
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
@@ -50,8 +39,7 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
-    count = 0
+   count = 0
     
     for letter in lettersGuessed:
         if letter in secretWord:
@@ -70,7 +58,6 @@ def getGuessedWord(secretWord, lettersGuessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
     '''
-    # FILL IN YOUR CODE HERE...
     wordUI = list(secretWord)
 
     for i in range(len(wordUI)):
@@ -87,7 +74,6 @@ def getAvailableLetters(lettersGuessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE...
     az = list("abcdefghijklmnopqrstuvwxyz")
 
     avail = sorted(set(az) - set(lettersGuessed))
@@ -115,7 +101,6 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
     # Game start and initial parameters
     print("Welcome to the game Hangman!")
     print("I am thinking of a word that is", len(secretWord), "letters long")
@@ -159,9 +144,7 @@ def hangman(secretWord):
         if  guesses == 0:
             print("Sorry, you ran out of guesses. The word was " + secretWord + ".")
         
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
 
+# Code testing
 secretWord = chooseWord(wordlist).lower()
 hangman(secretWord)
