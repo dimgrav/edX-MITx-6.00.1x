614abec63e7967ff9dca0d6deb0efb81768d6230
Dimitris Gravanis <dim_grav@yahoo.gr>
2017-04-02 19:58:06 +0300

    Update ps4b.py

--- problem set 4/ps4b.py
+++ problem set 4/ps4b.py
@@ -2,12 +2,7 @@
 import ps4a
 import time
 
-
-#
-#
 # Computer chooses a word
-#
-#
 def compChooseWord(hand, wordList, n):
     """
     Given a hand and a wordList, find the word that gives 
@@ -24,27 +19,18 @@ def compChooseWord(hand, wordList, n):
 
     returns: string or None
     """
-    # Create a new variable to store the maximum score seen so far (initially 0)
     bestScore = 0
-    # Create a new variable to store the best word seen so far (initially None)  
     bestWord = None
-    # For each word in the wordList
+    
     for word in wordList:
-        # If you can construct the word from your hand
         if isValidWord(word, hand, wordList):
-            # find out how much making that word is worth
             score = getWordScore(word, n)
-            # If the score for that word is higher than your best score
             if (score > bestScore):
-                # update your best score, and best word accordingly
                 bestScore = score
                 bestWord = word
-    # return the best word you found.
     return bestWord
 
-#
 # Computer plays a hand
-#
 def compPlayHand(hand, wordList, n):
     """
     Allows the computer to play the given hand, following the same procedure
@@ -64,43 +50,28 @@ def compPlayHand(hand, wordList, n):
     wordList: list (string)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     """
-    # Keep track of the total score
+    
     totalScore = 0
-    # As long as there are still letters left in the hand:
     while (calculateHandlen(hand) > 0) :
-        # Display the hand
         print("Current Hand: ", end=' ')
         displayHand(hand)
-        # computer's word
         word = compChooseWord(hand, wordList, n)
-        # If the input is a single period:
         if word == None:
-            # End the game (break out of the loop)
             break
             
-        # Otherwise (the input is not a single period):
         else :
-            # If the word is not valid:
             if (not isValidWord(word, hand, wordList)) :
                 print('This is a terrible error! I need to check my own code!')
                 break
-            # Otherwise (the word is valid):
             else :
-                # Tell the user how many points the word earned, and the updated total score 
                 score = getWordScore(word, n)
                 totalScore += score
-                print('"' + word + '" earned ' + str(score) + ' points. Total: ' + str(totalScore) + ' points')              
-                # Update hand and show the updated hand to the user
+                print('"' + word + '" earned ' + str(score) + ' points. Total: ' + str(totalScore) + ' points') 
                 hand = updateHand(hand, word)
                 print()
-    # Game is over (user entered a '.' or ran out of letters), so tell user the total score
     print('Total score: ' + str(totalScore) + ' points.')
 
-    
-#
-# Problem #6: Playing a game
-#
-#
+# Playing a game
 def playGame(wordList):
     """
     Allow the user to play an arbitrary number of hands.
@@ -169,9 +140,7 @@ def playGame(wordList):
                         compPlayHand(deal, wordList, HAND_SIZE)
                         break
         
-#
 # Build data structures used for entire session and play game
-#
 if __name__ == '__main__':
     wordList = loadWords()
     playGame(wordList)
