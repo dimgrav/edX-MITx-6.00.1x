9652bfa19307b85742f00d7bd913f27e515243b5
Dimitris Gravanis <dim_grav@yahoo.gr>
2017-04-03 21:55:02 +0300

    Update Problem5.py

--- midterm/Problem5.py
+++ midterm/Problem5.py
@@ -10,7 +10,6 @@ def dotProduct(listA, listB):
     listA: a list of numbers
     listB: a list of numbers of the same length as listA
     '''
-    # Your code here
     total = 0 
     
     for n in range(len(listA)):
